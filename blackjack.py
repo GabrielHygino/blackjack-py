cad3aa88e38b9d2a781f18163aca810b40e62ffb
Gabriel Hygino <62866108+GabrielHygino@users.noreply.github.com>
2023-02-28 19:55:28 -0300

    finished game logic

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,8 @@
 from json import loads
 from requests import get
+conta = 0
+total = 0
+mao = []
 
 def deck_id():
   response = get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")._content.decode()
@@ -9,48 +12,66 @@ def deck_id():
 def realeza(carta):
   if(carta == "QUEEN" or carta == "KING" or carta == "JACK"):
     carta = 10
-  else:
-     carta = carta
-  return carta
+  elif(carta == "ACE"):
+     carta = 11
+  return int(carta)
+
+def pegacarta(data, mao, conta, total):
+  for carta in data["cards"]:
+      total = realeza(carta.get("value"))
+      
+      conta += total
+      if realeza(carta.get("value")) == 11 and conta >= 22:
+        conta -= 10
+      mao.append(carta.get("value")) 
+
+ 
+  return mao, conta
+
+def resultado(conta):
+  if conta == 21:       
+    return print("Você ganhou!"), print("Fim de jogo!")
+  return print("Você perdeu :("), print("Fim de jogo!")
 
 def jogar():
   deck_atual = deck_id()
   response = get(f"https://www.deckofcardsapi.com/api/deck/{deck_atual}/draw/?count=2")
   data = loads(response._content.decode())
-  print("Suas cartas atuais são: ") 
-
-  mao = []
-  total = 0
-  for carta in data["cards"]:
-      mao += carta.get("value")
-      realeza(carta)
-      total += carta
-      
- 
-
-  print(mao)
-  print("O total da sua mão é " + total)
+  primeira_mao, somatoria = pegacarta(data, mao, conta, conta)
+  print("Suas cartas atuais são: ")
+  print(primeira_mao)
+  print("O total da sua mão é: " )
+  print(somatoria) 
 
   while True:
-    option = input("Deseja comprar mais alguma carta? Digite S para sim, N para não.")
-    if option == "S":
+    mao_atual = primeira_mao
+    conta_atual = somatoria
+
+    if conta_atual >= 21:
+       resultado(conta_atual)
+       break
+    
+    option = input("Deseja comprar mais alguma carta? Digite S para sim, N para não.").lower()
+    
+    if option == "s" or option == "sim": 
         response = get(f"https://www.deckofcardsapi.com/api/deck/{deck_atual}/draw/?count=1")
-        break
-    elif option == "N":
+        data = loads(response._content.decode())
+        mao_atual, conta_atual = pegacarta(data, mao_atual, conta_atual, conta_atual)
+        somatoria = conta_atual
+
+        print("Suas cartas atuais são: ")
+        print(mao_atual)
+        print("O total da sua mão é: ")
+        print(conta_atual)
+        
+        if conta_atual >= 21:
+           resultado(conta_atual)
+           break
+        
+    elif option == "n" or option == "nao":
+        resultado(conta_atual)
         break
     else:
        print("Digite S ou N")
-
-
-
-
- # if(comprar_cartas):  response = get(f"https://www.deckofcardsapi.com/api/deck/{deck_atual}/draw/?count=1")
-
-  #for carta in data["cards"]:
-  #  a = carta.get("value")
-   # print(a) 
-  
-
-
-
+       break
 jogar()
